01ffc72cdd6603b130d677796617bcc2842acf30
Matheus Capelanes <matheus.capelanes@outlook.com>
2024-12-24 16:38:49 -0300

    feat zap link and footer style

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,11 @@ module.exports = {
     "./src/**/*.{html,js}", 
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        'sans': ['Inter', 'sans-serif'],
+      },
+    },
   },
   plugins: [],
 }
